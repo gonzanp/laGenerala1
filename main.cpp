4646dd1053411e9ee4a8bee3984fe54880e9217b
gonzanp <gonza865@gmail.com>
2022-10-27 11:59:44 -0300

    cambios al menu de hoja main

--- main.cpp
+++ main.cpp
@@ -4,21 +4,106 @@
 #include<cstdio>
 #include<ctime>
 #include<conio.h>
-#include<cstring>
 #include"rlutil.h"
+#include<cstring>
 
 using namespace std;
 
 #include "funciones.h"
 #include "funcionesGenerales.h"
+
 int main(){
 int opc, puntaje= 0, tiradas= 0, puntajeMax= 0, tiradasMax = 0;
 char ganador[30], ganadorMax[30];
-rlutil::setBackgroundColor(rlutil::GREEN);
+//rlutil::setBackgroundColor(rlutil::BLACK);
+
+int op = 1, y = 0;
 
 
 while(true){
-    rlutil::cls();
+    //rlutil::cls();
+    rlutil::setBackgroundColor(rlutil::BLACK);
+    rlutil::setColor(rlutil::WHITE);
+    rlutil::hidecursor();
+
+    rlutil::locate(30,10);
+
+    cout<<" JUEGO NUEVO DE A 1."<<endl;
+    rlutil::locate(30,11);
+    cout<<" JUEGO NUEVO ENTRE 2."<<endl;
+    rlutil::locate(30,12);
+    cout<<" MOSTRAR PUNTUACION MAS ALTA."<<endl;
+    rlutil::locate(30,13);
+    cout<<" SALIR."<<endl;
+    showItem(" JUEGO NUEVO DE A 1.", 30, 10, y== 0);
+    showItem(" JUEGO NUEVO DE A 2.", 30, 11, y== 1);
+    showItem(" MOSTRAR PUNTUACION MAS ALTA.", 30, 12, y== 2);
+    showItem(" SALIR.", 30, 13, y== 3);
+
+    rlutil::locate(28,10 + y);
+    cout<<(char)175 <<endl;
+
+    int key = rlutil::getkey();
+    //cout<<key<<endl;
+    switch(key){
+        case 14: //arriba
+            rlutil::locate(28,10 + y);
+            cout<<" " <<endl;
+            y--;
+            if(y < 0){
+                y = 0;
+            }
+            break;
+        case 15: //abajo
+            rlutil::locate(28,10 + y);
+            cout<<" " <<endl;
+            y++;
+            if(y > 3){
+                y= 3;
+            }
+            break;
+        case 1: //enter
+            switch(y){
+                case 0 :
+                    PARTIDA(ganador, puntaje, tiradas);
+
+                    break;
+                case 1:
+                    PARTIDA2(ganador, puntaje, tiradas);
+                    break;
+                case 2:
+                    if(puntaje > puntajeMax){
+                        puntajeMax = puntaje;
+                        tiradasMax = tiradas;
+                        strcpy(ganadorMax,ganador);
+                    }
+                    cout<<"EL JUGADOR: "<<ganadorMax<<" POSEE EL RECORD DE PUNTUACION CON UN TOTAL DE: "<<puntajeMax<<" PUNTOS, EN "<<tiradasMax<<" TIRADAS."<<endl;
+                    system("pause");
+                    break;
+                case 3:
+                    return 0;
+                    break;
+
+
+            }
+
+            break;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
     cout<<"1- JUEGO NUEVO DE A 1."<<endl<<
     "2- JUEGO NUEVO ENTRE 2."<<endl<<
     "3- MOSTRAR PUNTUACION MAS ALTA."<<endl<<
@@ -48,6 +133,5 @@ default:
     rlutil::anykey();
     }
 }
-}
-
+}*/
 
