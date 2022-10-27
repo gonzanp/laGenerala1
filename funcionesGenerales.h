5ed81ee1369f44969dda85b5378ab8baf0ba0335
Nazuti <luquidiba@gmail.com>
2022-10-27 11:21:10 -0300

    Dados, rlutil

--- funcionesGenerales.h
+++ funcionesGenerales.h
@@ -2,17 +2,17 @@
 #define FUNCIONESGENERALES_H_INCLUDED
 
 
-void comenzarRonda(char nombre[30], int &puntos, int &tiradasGanador);
-void comenzarRonda2(char nombre[30], int &puntos, int &tiradasGanador);
+void PARTIDA(char nombre[30], int &puntos, int &tiradasGanador);
+void PARTIDA(char nombre[30], int &puntos, int &tiradasGanador);
 
 ////
-    void comenzarRonda(char nombre[30], int &puntos, int &tiradasGanador){
-    int dados[5], vPuntos[10], puntajeFinal[10],limite = 6, opc1, rondas=0, tiradasTotales=0, sumaPuntaje = 0, tiradas = 0;
-    bool ganoPartida = false, testeo = false, manual=false, generala=false;
-
+    void PARTIDA(char nombre[30], int &puntos, int &tiradasGanador){
+    int dados[5], vPuntos[10], puntajeFinal[10], opc1, rondas=0, tiradasTotales=0, sumaPuntaje = 0, tiradas = 0;
+    bool ganoPartida = false, testeo = false, manual=false;
+    rlutil::cls();
     cout<<"-INGRESE SU NOMBRE: ";
     cargarNombre(nombre);
-    system("cls");
+    rlutil::cls();
 
     cout<<"- INGRESE 1(UNO) PARA LA VERSION NORMAL DEL JUEGO."<<endl<<"- INGRESE 2(DOS) PARA LA VERSION DE TESTEO"<<endl;
     cin>>opc1;
@@ -29,16 +29,16 @@ case 1:
     manual=false;
     cargarAleatorio(dados, 5, 6);
     ordenarVector(dados, 5);
-    asignarValores(dados, 5, vPuntos, puntajeFinal, testeo, ganoPartida, manual, tiradas, rondas, nombre, sumaPuntaje, tiradasTotales);
+    RONDA(dados, 5, vPuntos, puntajeFinal, testeo, ganoPartida, manual, tiradas, rondas, nombre, sumaPuntaje, tiradasTotales);
 
     }
     if(ganoPartida == true){
     cout<<"FELICIDADES "<<nombre<<", OBTUVISTE GENERALA SERVIDA Y GANASTE CON UN PUNTAJE DE: "<<sumaPuntaje<<", CON UNOS TIROS TOTALES DE: "<<tiradasTotales<<endl;
-    system("pause");
+    rlutil::anykey();
     }
     if(rondas == 10){
     cout<<"TERMINASTE LA PARTIDA "<<nombre<<" CON UN PUNTAJE DE: "<<sumaPuntaje<<", CON UNOS TIROS TOTALES DE: "<<tiradasTotales<<endl;
-    system("pause");
+    rlutil::anykey();
     }
     break;
 
@@ -47,15 +47,15 @@ case 2:
     while(rondas < 10 && ganoPartida == false){
     rondas++;
     manual=true;
-    asignarValores(dados, 5, vPuntos, puntajeFinal, testeo, ganoPartida, manual, tiradas, rondas, nombre, sumaPuntaje, tiradasTotales);
+    RONDA(dados, 5, vPuntos, puntajeFinal, testeo, ganoPartida, manual, tiradas, rondas, nombre, sumaPuntaje, tiradasTotales);
     }
     if(ganoPartida == true){
     cout<<"FELICIDADES "<<nombre<<", OBTUVISTE GENERALA SERVIDA Y GANASTE CON UN PUNTAJE DE: "<<sumaPuntaje<<" CON UNOS TIROS TOTALES DE: "<<tiradasTotales<<endl;
-    system("pause");
+    rlutil::anykey();
     }
     if(rondas == 10){
     cout<<"TERMINASTE LA PARTIDA "<<nombre<<", CON UN PUNTAJE DE: "<<sumaPuntaje<<", CON UNOS TIROS TOTALES DE: "<<tiradasTotales<<endl;
-    system("pause");
+    rlutil::anykey();
     }
     break;
     }
@@ -63,8 +63,8 @@ case 2:
     tiradasGanador = tiradasTotales;
 }
 
-    void comenzarRonda2(char nombre[30], int &puntos, int &tiradasGanador){
-    int dados[5], vPuntos[10], puntajeFinal[10], puntajeFinal2[10], limite = 6, opc1, rondas=0, tiradasTotales=0, tiradasTotales2= 0, sumaPuntaje = 0, sumaPuntaje2 = 0, tiradas = 0;
+    void PARTIDA2(char nombre[30], int &puntos, int &tiradasGanador){
+    int dados[5], vPuntos[10], puntajeFinal[10], puntajeFinal2[10], rondas=0, tiradasTotales=0, tiradasTotales2= 0, sumaPuntaje = 0, sumaPuntaje2 = 0, tiradas = 0;
     char vNombre[30], vNombre2[30];
     bool ganoPartida = false, ganoPartida2 = false, manual=false, testeo=false;
 
@@ -82,12 +82,12 @@ case 2:
     rondas++;
     cargarAleatorio(dados, 5, 6);
     ordenarVector(dados, 5);
-    asignarValores(dados, 5, vPuntos, puntajeFinal, testeo, ganoPartida, manual, tiradas, rondas, vNombre, sumaPuntaje, tiradasTotales);
+    RONDA(dados, 5, vPuntos, puntajeFinal, testeo, ganoPartida, manual, tiradas, rondas, vNombre, sumaPuntaje, tiradasTotales);
 
     ///jugador 2
     cargarAleatorio(dados, 5, 6);
     ordenarVector(dados, 5);
-    asignarValores(dados, 5, vPuntos, puntajeFinal2, testeo, ganoPartida2, manual, tiradas, rondas, vNombre2, sumaPuntaje2, tiradasTotales2);
+    RONDA(dados, 5, vPuntos, puntajeFinal2, testeo, ganoPartida2, manual, tiradas, rondas, vNombre2, sumaPuntaje2, tiradasTotales2);
     }
     sumaPuntaje = sumarVector(puntajeFinal, 10);
     sumaPuntaje2 = sumarVector(puntajeFinal2, 10);
@@ -97,13 +97,13 @@ case 2:
     puntos = sumaPuntaje;
     tiradasGanador = tiradasTotales;
     strcpy(nombre, vNombre);
-    system("pause");
+    rlutil::anykey();
     }else{
     cout<<"EL GANADOR ES EL JUGADOR: "<<vNombre2<<" CON UN PUNTAJE DE: "<<sumaPuntaje2<<" CON UNOS TIROS TOTALES DE: "<<tiradasTotales2<<endl;
     puntos = sumaPuntaje2;
     tiradasGanador = tiradasTotales2;
     strcpy(nombre, vNombre2);
-    system("pause");
+    rlutil::anykey();
     }
 }
 
